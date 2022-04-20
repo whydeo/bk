db205544c0c756f527da0af480f2308a8c2d4563
whydeo <meanwhy189@gmail.com>
2022-04-20 16:58:17 +0700

    oke

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -6,7 +6,7 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
             <div class="container">
                 <div class="row">
-                    <form action="/" method="post" enctype="multipart/form-data">
+                    <form action="{{route('import')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="input-group mb-3">
                             <input type="file" name="file" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
@@ -28,7 +28,7 @@
                             @php
                             $no = 0;
                             @endphp
-                            @foreach ($siswa as $siswa)
+                            @forelse ($siswa as $siswa)
                             <tr>
                                 <th scope="row">{{ ++$no }}</th>
                                 <td>{{ $siswa->nama_siswa }}</td>
